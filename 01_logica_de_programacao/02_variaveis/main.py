37a4019e7e266402bfd32c4ecca64b07a67de674
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-10-17 16:48:11 -0300

    Meu primeiro commit. Aula do dia 17/10/2025.

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome_completo = "Alex Machado" # string
+idade = 40 # int
+altura = 1.72 # float
+programador = True # bool (boolean)
+
+# saída de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programacao/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome = "Alex Machado"
+idade = 40
+altura = 1.72
+programador = True
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
