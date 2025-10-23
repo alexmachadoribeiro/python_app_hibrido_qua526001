31ae69c5b3f82d275852302dcc7f2c8f58130be8
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-10-23 17:14:39 -0300

    Aula do dia 23/10/2025.

---
+++ 02_imports/01_limpa_console/main.py
@@ -0,0 +1,41 @@
+# importação de bibliotecas
+import os
+
+# loop
+while True:
+    # limpeza de console
+    os.system("cls")
+
+    # tratamento de exceção
+    try:
+        # entrada de dados
+        nome = input("Informe o nome: ").strip().title()
+        email = input("Informe o e-mail: ").strip().lower()
+        cpf = input("Informe o CPF: ").strip()
+
+        # limpeza de console
+        os.system("cls")
+
+        # saída de dados
+        print(f"Nome: {nome}")
+        print(f"E-mail: {email}")
+        print(f"CPF: {cpf}")
+
+        # menu
+        print("1 - Inserir novos dados.")
+        print("2 - Sair do programa.")
+
+        opcao = input("Opção desejada: ").strip()
+
+        # verifica a opção escolhida
+        match opcao:
+            case "1":
+                continue
+            case "2":
+                print("Programa encerrado.")
+                break
+            case _:
+                print("Opção inválida.")
+                break
+    except:
+        print("Não foi possível receber informações.")
