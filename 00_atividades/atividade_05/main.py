ebbaf5bb77446c8a6aa32abfd0cc02b8274ae7e5
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-10-28 17:38:11 -0300

    Aula do dia 28/10/2025.

---
+++ 00_atividades/atividade_05/main.py
@@ -0,0 +1,33 @@
+# TODO: atividade
+
+estados = [
+    "Distrito Federal",
+    "Bahia",
+    "Acre",
+    "Espírito Santo",
+    "Ceará",
+    "Goiás",
+    "Rio Grande do Sul",
+    "Tocantins",
+    "Pernambuco",
+    "São Paulo",
+    "Santa Catarina",
+    "Amazonas",
+    "Roraima",
+    "Alagoas",
+    "Sergipe",
+    "Paraná",
+    "Amapá",
+    "Rio de Janeiro",
+    "Rio Grande do Norte"
+]
+
+estados.sort()
+
+print("\nEstados Brasileiros:\n")
+for estado in estados:
+    print(estado)
+
+"""
+Crie uma lista com os nomes de todos os estados brasileiros, e mostre na tela os estados em ordem alfabética.
+"""
