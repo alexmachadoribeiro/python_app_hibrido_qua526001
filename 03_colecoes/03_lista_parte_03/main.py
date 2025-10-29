8c25fcc977a2c662eff328b94022b48e8b8459d1
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-10-29 17:05:18 -0300

    Aula do dia 29/10/2025.

---
+++ 03_colecoes/03_lista_parte_03/main.py
@@ -0,0 +1,50 @@
+# biblioteca
+import os
+
+# declaração de lista
+nomes = []
+
+# limpa o console
+os.system("cls")
+
+# loop
+while True:
+    # menu
+    print("1 - Inserir novo nome")
+    print("2 - Exibir lista de nomes")
+    print("3 - Excluir nome na lista")
+    print("4 - Sair do programa")
+    opcao = input("Informe a opção desejada: ").strip()
+    match opcao:
+        case "1":
+            os.system("cls")
+            novo_nome = input("Informe o novo nome: ").strip().title()
+            nomes.append(novo_nome) # insere novo nome na lista
+            os.system("cls")
+            print(f"{novo_nome} cadastrado com sucesso.")
+            continue
+        case "2":
+            os.system("cls")
+            print("Lista de nomes:\n")
+            for i in range(len(nomes)):
+                print(f"{i} - {nomes[i]}")
+            print(f"\n{'-'*40}\n")
+            continue
+        case "3":
+            os.system("cls")
+            try:
+                posicao = int(input("Informe a posição a ser excluída: ").strip())
+                if posicao >= 0 and posicao < len(nomes):
+                    del(nomes[posicao])
+                    print("Nome excluído com sucesso.")
+                else:
+                    print("Posição inexistente.")
+            except Exception as e:
+                print(f"Posição inválida. {e}.")
+            continue
+        case "4":
+            print("Programa encerrado.")
+            break
+        case _:
+            print("Opção inválida.")
+            continue
---
+++ 03_colecoes/04_tupla/main.py
@@ -0,0 +1,14 @@
+# declaração de tupla
+dias_semana = (
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sábado"
+)
+
+# saída de dados
+for dia in dias_semana:
+    print(dia)
