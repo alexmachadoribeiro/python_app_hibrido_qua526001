2671c3f4e6e5aaf3cfc4c083d31c6b59d58c3966
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-11-18 17:39:18 -0300

    Aula do dia 18/11/2025.

--- 06_banco_de_dados/01_crud/main.py
+++ 06_banco_de_dados/01_crud/main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from entidades import criar_tb_pessoa
-from modulo import limpar
+from modulo import limpar, cadastrar
 
 def main():
     engine = create_engine("sqlite:///01_crud/database/crud.db")
@@ -12,7 +12,22 @@ def main():
     session = Session()
 
     limpar()
-    # TODO: fazer o CRUD
+    while True:
+        print(f"{'-'*20} 🐍 CRUD DA COBRA 🐍 {'-'*20}\n")
+        print("0 - Sair do programa")
+        print("1 - Cadastrar nova pessoa")
+        opcao = input("Opção desejada: ").strip()
+        limpar()
+        match opcao:
+            case "0":
+                print("Programa encerrado.")
+                break
+            case "1":
+                print(cadastrar(session, Pessoa))
+                continue
+            case _:
+                print("Opção inválida.")
+                continue
 
     session.close()
 
--- 06_banco_de_dados/01_crud/modulo.py
+++ 06_banco_de_dados/01_crud/modulo.py
@@ -1,4 +1,36 @@
 import os
+from datetime import datetime
 
 def limpar():
     os.system("cls" if os.name == "nt" else "clear")
+
+def cadastrar(session, Pessoa):
+    try:
+        nome = input("Informe o nome: ").strip().title()
+        genero = input("Informe o gênero: ").strip().lower()
+        nascimento = input("Informe a data de nascimento (dd/mm/aaaa): ").strip()
+        nascimento = datetime.strptime(nascimento, "%d/%m/%Y").date()
+        email = input("Informe o e-mail: ").strip().lower()
+
+        # filtra atributo email das pessoas que possuem mesmo e-mail informado
+        pessoas = session.query(Pessoa).filter(Pessoa.email.like(email)).all()
+
+        if email in [pessoa.email for pessoa in pessoas]:
+            return "E-mail já cadastrado."
+        else:
+            # instancia do objeto
+            nova_pessoa = Pessoa(
+                nome=nome, 
+                nascimento=nascimento,
+                email=email,
+                genero=genero
+            )
+
+            # insert into pessoa
+            session.add(nova_pessoa)
+            session.commit()
+
+            return f"{nova_pessoa.nome} cadastrada com sucesso."
+
+    except Exception as e:
+        print(f"Não foi possível cadastrar. {e}.")
