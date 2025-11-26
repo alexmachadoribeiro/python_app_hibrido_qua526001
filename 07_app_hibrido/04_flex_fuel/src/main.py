b59478f93e86d49113ff645157eda4e128269419
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-11-26 17:39:56 -0300

    Aula do dia 26/11/2025.

---
+++ 07_app_hibrido/04_flex_fuel/src/main.py
@@ -0,0 +1,44 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "App Flex Fuel"
+    page.scroll = "adaptive"
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    gasolina = ft.TextField(
+        label="Valor da gasolina",
+        prefix_text="R$ ",
+        keyboard_type=ft.KeyboardType.NUMBER
+    )
+    etanol = ft.TextField(
+        label="Valor do etanol",
+        prefix_text="R$ ",
+        keyboard_type=ft.KeyboardType.NUMBER
+    )
+    dlg_modal = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Melhor abastecer com:"),
+        content=ft.Text(size=20, weight="bold"),
+        actions=[ft.TextButton("OK", on_click=lambda e:page.close(dlg_modal))],
+        actions_alignment=ft.MainAxisAlignment.END
+    )
+    
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                ft.Text("FLEX FUEL", size=35, weight="bold"),
+                alignment=ft.alignment.center,
+            ),
+            # expand=True,
+        ),
+        ft.ResponsiveRow(
+            [
+                ft.Container(gasolina, col={"sm": 6, "md": 4, "xl": 2}),
+                ft.Container(etanol, col={"sm": 6, "md": 4, "xl": 2})
+            ]
+        )
+    )
+
+
+ft.app(main)
